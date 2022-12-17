83cb0604a7e3034a6950769f4aad4897c71d3831
Backflipper666 <elderly619@gmail.com>
2022-12-17 17:47:25 +0600

    style Button component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import Filters from './components/Filters/Filters'
 import Tabs from './components/Tabs/Tabs'
 import FlightList from './components/FlightList/FlightList'
 import styles from './App.module.scss'
+import plane from './assets/plane.svg'
+import Button from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <header className={styles.header}>
+        <img src={plane} className={styles.plane} />
+      </header>
+
       <div className={styles.content}>
         <Filters />
         <main>
           <Tabs />
           <FlightList />
+          <Button />
         </main>
       </div>
     </div>
