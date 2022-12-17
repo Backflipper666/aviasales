28f63559936aebda219a20a0f92af7e85332f8a7
Backflipper666 <elderly619@gmail.com>
2022-12-17 17:24:46 +0600

    style Filters component

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -4,32 +4,32 @@ const Filters = () => {
   return (
     <fieldset className={styles.fieldset}>
       <h5 className={styles.header}>количество пересадок</h5>
-      <div>
-        <input type="checkbox" id="all" name="all" />
+      <div className={styles.wrapper}>
+        <input type="checkbox" id="all" name="all" className={styles.checkbox} />
         <label htmlFor="all" className={styles.label}>
           Все
         </label>
       </div>
-      <div>
-        <input type="checkbox" id="without" name="without" />
+      <div className={styles.wrapper}>
+        <input type="checkbox" id="without" name="without" className={styles.checkbox} />
         <label htmlFor="without" className={styles.label}>
           Без пересадок
         </label>
       </div>
-      <div>
-        <input type="checkbox" id="one" name="one" />
+      <div className={styles.wrapper}>
+        <input type="checkbox" id="one" name="one" className={styles.checkbox} />
         <label htmlFor="one" className={styles.label}>
           1 пересадка
         </label>
       </div>
-      <div>
-        <input type="checkbox" id="two" name="two" />
+      <div className={styles.wrapper}>
+        <input type="checkbox" id="two" name="two" className={styles.checkbox} />
         <label htmlFor="two" className={styles.label}>
           2 пересадки
         </label>
       </div>
-      <div>
-        <input type="checkbox" id="three" name="three" />
+      <div className={styles.wrapper}>
+        <input type="checkbox" id="three" name="three" className={styles.checkbox} />
         <label htmlFor="three" className={styles.label}>
           3 пересадки
         </label>
